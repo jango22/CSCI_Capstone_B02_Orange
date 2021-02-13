1965102fb7f761ac90567a4069715a9d2723b83a
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-02-13 18:17:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -28,3 +28,27 @@ const port = process.env.port || 8080;
 server.listen(port, () => {
     console.log("Sever console log.")
 });
+
+<html>
+<body>
+
+<p>Click the button labeled "Log Time" to store the time you pressed the button.</p>
+
+<button onclick="LodCurrentTime()">Log Time</button>
+
+<button onclick="lblTime"></button>
+
+<script>
+    function LogCurrentTime() {
+        var date = new Date();
+        var hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+        var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+        var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+        time = hours + ":" + minutes + ":" + seconds;
+        document.getElementById("lblTime").innerHTML = time;
+
+    };
+</script>
+
+</body>
+</html>
