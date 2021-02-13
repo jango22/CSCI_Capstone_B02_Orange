c8858ae5277c4cafa1a5867db31b2b82554efcb3
Nathan Bennett <jango.bennett@gmail.com>
2021-02-13 15:25:42 -0500

    Addes javascript

---
+++ app.js
@@ -0,0 +1,30 @@
+
+/*
+const express = require('express');
+
+// const app = express();
+
+const port = 80;
+
+app.get('/', (req, res) => {
+    res.sendFile('./www/index.html', { root: __dirname });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
+
+*/
+
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = process.env.port || 8080;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
