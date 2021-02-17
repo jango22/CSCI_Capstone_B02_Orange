5074d6f03be78687094477c15545fa128ce6b8af
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-02-17 16:34:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ server.listen(port, () => {
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: "csci2999b02.cps316w6axpe.us-east-1.rds.amazonaws.com,1433",
+    host: "csci2999b02.cps316w6axpe.us-east-1.rds.amazonaws.com",
     user: "orangeteamadmin",
     password: "AevhKutuIA7luKby4JZV"
 });
