8075cbd97a9adcd833dafce4d51560f192fda640
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-02-17 17:48:23 -0500

    test

---
+++ Script1.js
@@ -0,0 +1,10 @@
+var sql = require(‘mssql’);
+
+var config = {
+
+user: “orangeteamadmin”,
+password: “AevhKutuIA7luKby4JZV”,
+server: “csci2999b02.cps316w6axpe.us-east-1.rds.amazonaws.com”,
+database: “csci2999b02”
+
+}
