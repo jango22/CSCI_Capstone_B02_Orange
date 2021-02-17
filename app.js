ba5d25c5e8fbc786c2a136723f0c0696000f5db8
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-02-17 13:00:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -38,4 +38,26 @@ server.listen(port, () => {
     console.log("Sever console log.")
 });
 
-
+var Connection = require('tedious').Connection;  
+    var config = {  
+        server: 'aa96jyhrersobx.cps316w6axpe.us-east-1.rds.amazonaws.com,1433',  //update me
+        authentication: {
+            type: 'default',
+            options: {
+                userName: 'orangeteam', //update me
+                password: 'capstone02'  //update me
+            }
+        },
+        options: {
+            // If you are on Microsoft Azure, you need encryption:
+            encrypt: true,
+            database: 'CSCI299B02'  //update me
+        }
+    };  
+    var connection = new Connection(config);  
+    connection.on('connect', function(err) {  
+        // If no error, then good to proceed.
+        console.log("Connected");  
+    });
+    
+    connection.connect();
