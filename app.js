3e4ecc505dca92d36a81abbfc209ca02a49fef27
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-02-17 14:25:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ server.listen(port, () => {
 
 var Connection = require('tedious').Connection;  
     var config = {  
-        server: 'csci2999b02.cps316w6axpe.us-east-1.rds.amazonaws.com',  
+        server: 'csci2999b02.cps316w6axpe.us-east-1.rds.amazonaws.com,8080',  
         authentication: {
             type: 'default',
             options: {
