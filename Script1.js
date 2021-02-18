0d827e4b1ab6cd23e4c8be5f483dbb4cdfe778cf
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-02-17 20:58:39 -0500

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -1,10 +1,10 @@
-var sql = require(‘mssql’);
+var sql = require(â€˜mssqlâ€™);
 
 var config = {
 
-user: “orangeteamadmin”,
-password: “AevhKutuIA7luKby4JZV”,
-server: “csci2999b02.cps316w6axpe.us-east-1.rds.amazonaws.com”,
-database: “csci2999b02”
+user: 'orangeteamadmin',
+password: 'AevhKutuIA7luKby4JZV',
+server: 'csci2999b02.cps316w6axpe.us-east-1.rds.amazonaws.com',
+database: 'csci2999b02'
 
 }
