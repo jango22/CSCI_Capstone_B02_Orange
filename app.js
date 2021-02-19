158ad24ff12b4b51d2ccd64cdc01d37baa440b61
Nathan Bennett <jango.bennett@gmail.com>
2021-02-18 22:11:12 -0500

    Dynamic web server

--- app.js
+++ app.js
@@ -1,43 +1,12 @@
-
-/*
 const express = require('express');
 
-// const app = express();
 
-const port = 80;
+const app = express(); //initializes the express application
+const port = 8080; //Sets the Port the express server runs on
 
+//on a request coming in...
+//if they request / (aka the homepage)
 app.get('/', (req, res) => {
-    res.sendFile('./www/index.html', { root: __dirname });
+   res.sendFile('./button.html', { root: __dirname }); // send them button.html
 });
-
 app.listen(port, () => console.log(`listening on port ${port}!`));
-
-*/
-
-
-const http = require('http');
-const express = require('express');
-const app = express();
-
-var realurl = "string";
-
-console.log(realurl)
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  
-  //window.location.href = realurl;
-
-  res.end('Hello World');
-  //res.redirect('/html');
-
-  });
-//THIS IS A CHANGE
-
-
-const port = process.env.port || 8080;
-server.listen(port, () => {
-    console.log("Sever console log.")
-});
-
