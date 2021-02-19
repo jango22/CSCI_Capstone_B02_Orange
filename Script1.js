4beb1f445ffa2aa6213048913a7a19004c9a1509
Nathan Bennett <jango.bennett@gmail.com>
2021-02-19 00:28:07 -0500

    Functional connection string

--- Script1.js
+++ Script1.js
@@ -6,27 +6,25 @@ var config = {
 }
 var express = require('express');
 var app = express();
-
-app.get('/', function (req, res) {
    
-    var sql = require('mssql');
-
-    // config for your database
-    var config = {
-        user: 'orangeteamadmin',
-        password: 'AevhKutuIA7luKby4JZV',
+var sql = require('mssql');
+// config for your database
+var config = {
+        user: "orangeteamadmin",
+        password: 'capstone02',
         server: 'csci2999b02.cps316w6axpe.us-east-1.rds.amazonaws.com',
-        database: 'csci2999b02'
-    };
+        //port: 1433
 
+    };
     // connect to your database
-    sql.connect(config, function (err) {
     
-        if (err) console.log(err);
+sql.connect(config, function (err) {
+    
+    if (err) console.log(err);
 
         // create Request object
-        var request = new sql.Request();
-           
+    var request = new sql.Request();
+    
         // query to the database and get the records
         request.query('select * from Student', function (err, recordset) {
             
@@ -36,8 +34,9 @@ app.get('/', function (req, res) {
             res.send(recordset);
             
         });
+    
     });
-});
+    
 
 var server = app.listen(5000, function () {
     console.log('Server is running..');
