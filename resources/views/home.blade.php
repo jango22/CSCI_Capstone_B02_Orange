44ca6555f5814a1ff6dedbc22d73ed30206e36ad
MasDijkstra <dijkstramas@gmail.com>
2021-02-22 23:46:47 -0500

    let's try

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 			
 		<header>
 		
-		<img src = "{{ asset('img/ndolt.png') }}" height ="100" width ="100" title="Logo" alt="Logo">
+		<img src = "{{ asset('../../public/img/ndolt.png') }}" height ="100" width ="100" title="Logo" alt="Logo">
 		<h1>Nuts &#38; Dolts</h1>
 		
 		  </div>
