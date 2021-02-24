30eae397197189ac4ea471f6cdd9c97b774b539c
MasDijkstra <dijkstramas@gmail.com>
2021-02-23 22:36:09 -0500

    Rudimentary alert message

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -4,10 +4,16 @@
 <head>
     <meta charset="utf-8">
     <title>Add a Product</title>
-    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
+@if(session()->has('successmessage'))
+    <div class="alert alert-success alert-dismissable fade show" role="alert">
+        {{ session('successmessage') }}
+    </div>
+@endif
 
 <h2>Add a Product to Inventory</h2>
 <form method="POST">
@@ -39,8 +45,6 @@
 
     //Get values from POST
     if(isset($_POST['submit'])){
-        echo var_dump($_POST);
-        echo($_POST['name']);
         $name = $_POST['name'];
         $SKU = $_POST['SKU'];
         $price = $_POST['price'];
@@ -48,6 +52,9 @@
 
         //Insert values into INVENTORY table
         $conn->query("INSERT INTO INVENTORY (name, productSKU, price, itemdesc) VALUES ('$name', $SKU, $price, '$desc');");
+
+        //Send success message to user
+        return redirect()->route('add')->with('successmessage','Added to inventory successfully.');
     }
 ?>
 
