41c2f5c3f8dc478b37ee7fe97a8c8c2e88c2c8b6
MasDijkstra <dijkstramas@gmail.com>
2021-03-02 20:12:01 -0500

    csrf fix fix

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-use Symfony\Component\HttpFoundation\Cookie;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -15,25 +14,4 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
-    /**
-     * Add the CSRF token to the response cookies.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Http\Response  $response
-     * @return \Illuminate\Http\Response
-     */
-    protected function addCookieToResponse($request, $response)
-    {
-    $config = config('session');
-
-    $response->headers->setCookie(
-        new Cookie(
-            'XSRF-TOKEN', $request->session()->token(), Carbon::now()->getTimestamp() + 60 * $config['lifetime'],
-            $config['path'], $config['domain'], $config['secure'], false
-        )
-    );
-
-    return $response;
-    }
 }
-?>
