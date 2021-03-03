b2716ebbeebd2b7e0c83c162b4acffa6d589d721
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-02 20:21:37 -0500

    Update HProducts.blade.php

--- resources/views/HProducts.blade.php
+++ resources/views/HProducts.blade.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
-	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 		<div class="w3-top">
 			<div class="w3-bar w3-green w3-card">
