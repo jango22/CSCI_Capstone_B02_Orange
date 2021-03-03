fdd2b4e965a62d30f2bc004a37496231a6849d24
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-02 23:04:40 -0500

    Update ContUs.blade.php

--- resources/views/ContUs.blade.php
+++ resources/views/ContUs.blade.php
@@ -60,7 +60,19 @@
 
 
     </form>
-   
+   <?php
+   $servername = "aa189btph88nlyp.cps316w6axpe.us-east-1.rds.amazonaws.com";
+    $username = "orangeadmin";
+    $password = "capstone02";
+    $dbname = "OrangeDB";
+    $port = "1433";
+    $conn = new PDO("sqlsrv:Server=$servername,$port;Database=$dbname;", $username, $password); 
+    
+        if (isset($_POST["message"])) {
+        mail("nutsandboltsb02@gmail.com", "Here is the subject line",
+        $_POST["insert your message here"], "From: nandbCustComplaint@gmail.com");
+        }
+    ?>
 	</div>
 	</div>
  	</main>
