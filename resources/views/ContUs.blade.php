2aff080f9b831a0a0a54d8f4e276c8826c9ea044
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-02 23:07:29 -0500

    Update ContUs.blade.php

--- resources/views/ContUs.blade.php
+++ resources/views/ContUs.blade.php
@@ -1,9 +1,16 @@
 <?php
+   $servername = "aa189btph88nlyp.cps316w6axpe.us-east-1.rds.amazonaws.com";
+    $username = "orangeadmin";
+    $password = "capstone02";
+    $dbname = "OrangeDB";
+    $port = "1433";
+    $conn = new PDO("sqlsrv:Server=$servername,$port;Database=$dbname;", $username, $password); 
+    
         if (isset($_POST["message"])) {
         mail("nutsandboltsb02@gmail.com", "Here is the subject line",
         $_POST["insert your message here"], "From: nandbCustComplaint@gmail.com");
         }
-  ?>
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -60,19 +67,7 @@
 
 
     </form>
-   <?php
-   $servername = "aa189btph88nlyp.cps316w6axpe.us-east-1.rds.amazonaws.com";
-    $username = "orangeadmin";
-    $password = "capstone02";
-    $dbname = "OrangeDB";
-    $port = "1433";
-    $conn = new PDO("sqlsrv:Server=$servername,$port;Database=$dbname;", $username, $password); 
-    
-        if (isset($_POST["message"])) {
-        mail("nutsandboltsb02@gmail.com", "Here is the subject line",
-        $_POST["insert your message here"], "From: nandbCustComplaint@gmail.com");
-        }
-    ?>
+   
 	</div>
 	</div>
  	</main>
