d11fd6d18635aeeca5d1b272275320a499211d24
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-02 21:07:25 -0500

    Update ContUs.blade.php

--- resources/views/ContUs.blade.php
+++ resources/views/ContUs.blade.php
@@ -1,54 +1,51 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Contact Us</title>
-	
-			<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-		    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-			<link rel="icon" href="pics/ndolts.ico" type="fav/ico">
-		</head>
-		<body>
-		<header>
-		<div class="jumbotron-fluid">
-		<div class="container">
-		
-			<!--Banner for website-->	
-			
-		
-		
-			<img class="feature" src = "https://i.imgur.com/uVymdir.png" height ="100" width ="100" title="Logo" alt="Logo">
-		<h1>Nuts &#38; Bolts</h1>
-		
-		  </div>
-		  </div>
-		  
-	</header>
-	
-	<!--Navigation Bar-->
-			<nav>
-	<ul>
-		<li><a href="HomePg.html">Home</a></li>
-		<li><a href="HProducts.html">HardCode Products</a></li>
-		<li><a href="ContUs.html">Contact Us</a></li>
-		<li><a href="FAQ.html">FAQ</a></li>
-	</ul>
-			</nav>
-			
-			<div class="content" id="wrapper">
-	<!--Main Body for Website-->
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<div class="w3-top">
+			<div class="w3-bar w3-green w3-card">
+				<a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+				<a href="HomePg.html" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+				<a href="HProducts.html" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Products</a>
+				<a href="ContUs.html" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Contact Us</a>
+				<a href="FAQ.html" class="w3-bar-item w3-button w3-padding-large w3-hide-small">FAQ</a>
+				<a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+			</div>
+		</div>
+		<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
+			<a href="HProducts.html" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Products</a>
+			<a href="ContUs.html" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Contact Us</a>
+			<a href="FAQ.html" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">FAQ</a>
+		</div>
+		<br> <br>
+		<title>HardCode Products</title>
 
-	
-		<main class="flex">
-			<div class="container">
+		<div class="jumbotron-fluid container">
+			<header>
+				<img src="pics/ndolt.png" height="150" width="150" title="Logo" alt="Logo" class="center">
+			</header>
+		</div>
+
+</head>
+<body>
+			
+			<div >	
+<main class="flex">
+		<div class="content container" id="wrapper">
+     <h1>Contact Us</h1>      
 	<h3>Where to Find Us?</h3>
-	<p>We are always open for any issues you have! Our phone line is always open!</p>
-	<p>Contact Us <br>Phone Number: 383-295-5524 <br>Email: customerservice@nutsandbolts.com <br>Located at: 5469 South Ridsweth Rd, Marion OH, 39455 </p>
-	</div>
-	
-	</main>
-	</body>
+	    <p>We are always open for any issues you have! Our phone line is always open!</p>
+	    <p>Contact Us <br>Phone Number: 383-295-5524 <br>Email: customerservice@nutsandbolts.com <br>Located at: 5469 South Ridsweth Rd, Marion OH, 39455 </p>
 	</div>
 	</div>
+ 	</main>
+ </body>
 <footer>
 </footer>
 </html>
