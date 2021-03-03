2452e1e377766e81c96ba3c3718916c586a3e27a
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-02 20:25:37 -0500

    Update HProducts.blade.php

--- resources/views/HProducts.blade.php
+++ resources/views/HProducts.blade.php
@@ -10,7 +10,7 @@
 		<div class="w3-top">
 			<div class="w3-bar w3-green w3-card">
 				<a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
-				<a href="/Welcome" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+				<a href="/welcome" class="w3-bar-item w3-button w3-padding-large">HOME</a>
 				<a href="/HProducts" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Products</a>
 				<a href="/Contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Contact Us</a>
 				<a href="/FAQ" class="w3-bar-item w3-button w3-padding-large w3-hide-small">FAQ</a>
