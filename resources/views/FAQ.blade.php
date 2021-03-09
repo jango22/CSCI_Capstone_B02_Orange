a4d131a22a22fc520f1ab169ab9c85f827e79265
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-09 17:22:55 -0500

    Update FAQ.blade.php

--- resources/views/FAQ.blade.php
+++ resources/views/FAQ.blade.php
@@ -42,164 +42,23 @@
                 
           <!-- FQA START HERE -->
                 
-     <script> <!-- FQA Script START HERE -->   
-var ButtonExpand = function (domNode) {
-
-  this.domNode = domNode;
-
-  this.keyCode = Object.freeze({
-    'RETURN': 13
-  });
-};
-
-ButtonExpand.prototype.init = function () {
-
-  this.controlledNode = false;
-
-  var id = this.domNode.getAttribute('aria-controls');
-
-  if (id) {
-    this.controlledNode = document.getElementById(id);
-  }
-
-  this.domNode.setAttribute('aria-expanded', 'false');
-  this.hideContent();
-
-  this.domNode.addEventListener('keydown',    this.handleKeydown.bind(this));
-  this.domNode.addEventListener('click',      this.handleClick.bind(this));
-  this.domNode.addEventListener('focus',      this.handleFocus.bind(this));
-  this.domNode.addEventListener('blur',       this.handleBlur.bind(this));
-
-};
-
-ButtonExpand.prototype.showContent = function () {
-
-  if (this.controlledNode) {
-    this.controlledNode.style.display = 'block';
-  }
-
-};
-
-ButtonExpand.prototype.hideContent = function () {
-
-  if (this.controlledNode) {
-    this.controlledNode.style.display = 'none';
-  }
-
-};
-
-ButtonExpand.prototype.toggleExpand = function () {
-
-  if (this.domNode.getAttribute('aria-expanded') === 'true') {
-    this.domNode.setAttribute('aria-expanded', 'false');
-    this.hideContent();
-  }
-  else {
-    this.domNode.setAttribute('aria-expanded', 'true');
-    this.showContent();
-  }
-
-};
-
-/* EVENT HANDLERS */
-
-ButtonExpand.prototype.handleKeydown = function (event) {
-
-  console.log('[keydown]');
-
-  switch (event.keyCode) {
-
-    case this.keyCode.RETURN:
-
-      this.toggleExpand();
-
-      event.stopPropagation();
-      event.preventDefault();
-      break;
-
-    default:
-      break;
-  }
-
-};
-
-ButtonExpand.prototype.handleClick = function (event) {
-  this.toggleExpand();
-};
-
-ButtonExpand.prototype.handleFocus = function (event) {
-  this.domNode.classList.add('focus');
-};
-
-ButtonExpand.prototype.handleBlur = function (event) {
-  this.domNode.classList.remove('focus');
-};
-
-/* Initialize Hide/Show Buttons */
-
-window.addEventListener('load', function (event) {
-
-  var buttons =  document.querySelectorAll('button[aria-expanded][aria-controls]');
-
-  for (var i = 0; i < buttons.length; i++) {
-    var be = new ButtonExpand(buttons[i]);
-    be.init();
-  }
-
-}, false);</script>   <!-- FQA Script END HERE --> 
-        
-        
-        
-        
-       <dl class="faq">
-  <dt>
-    <button aria-expanded="false" aria-controls="faq1_desc">
-      Store hours?
-    </button>
-  </dt>
-  <dd>
-      
-
- <table id="faq1_desc" class="desc">
-<tr><th>Sunday</th><td>Closed</td></tr>
-<tr><th>Monday</th><td>9am - 5pm</td></tr>
-<tr><th>Tuesday</th><td>9am - 5pm</td></tr>
-<tr><th>Wednesday</th><td>9am - 5pm</td></tr>
-<tr><th>Thursday</th><td>9am - 5pm</td></tr>
-<tr><th>Friday</th><td>9am - 5pm</td></tr>
-<tr><th>Saturday</th><td>9am - 5pm</td></tr>
-</table>
-  </dd>
-    
-        
-  <dt>
-    <button aria-expanded="false" aria-controls="faq2_desc">
-      Return Policy?
-    </button>
-  </dt>
-  <dd>
-      
-    <h6 id="faq2_desc" class="desc"> We accept return and cancellations under the following conditions:
-      
-      <ul id="faq2_desc" class="desc">
-  <li>Contact me within: 7 days of delivery</li>
-  <li>Ship items back within: 14 days of delivery</li>
-  <li>Request a cancellation within: 24 hours of purchase</li>
-</ul> 
-      </h6> 
-  </dd>
-           
-  <dt>
-    <button aria-expanded="false" aria-controls="faq3_desc">
-      Why the owner created the first Nuts and Bolts store?
-    </button>
-  </dt>
-  <dd>
-    <p id="faq3_desc" class="desc">
-      To let the customer do shopping 24/7. 
-    </p>
-  </dd>  
-   <!-- FQA END HERE -->               
+     <div class="row">
+  <div class="column side">
+    <h2>Side</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
+  </div>
+  
+  <div class="column middle">
+    <h2>Main Content</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+  </div>
+  
+  <div class="column side">
+    <h2>Side</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit..</p>
+  </div>
+</div>              
                                
 <footer>
 </footer>
