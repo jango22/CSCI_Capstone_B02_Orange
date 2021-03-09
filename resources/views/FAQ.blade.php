af6bc4bab5541b70b25bc8b3fda8373640bc5e49
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-09 17:16:29 -0500

    Update FAQ.blade.php

--- resources/views/FAQ.blade.php
+++ resources/views/FAQ.blade.php
@@ -1,37 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+<!DOCTYPE html>
+<html style="background-color: rgba(195,195,195);" lang="en">
 <head>
-	<title>FAQ</title>
 	
-  <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-		    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-		<link rel="icon" href="pics/ndolts.ico" type="fav/ico">
-		<div class="jumbotron-fluid">
-		<div class="container">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+	<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<div class="w3-top">
+			<div class="w3-bar w3-blue-gray w3-card">
+				<a class="w3-bar-item w3-button w3-padding-large w3-hide-medium w3-hide-large w3-right" href="javascript:void(0)" onclick="myFunction()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+				<a href="/" class="w3-bar-item w3-button w3-padding-large">HOME</a>
+				<a href="/HProducts" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Products</a>
+				<a href="/Contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Contact Us</a>
+				<a href="/FAQ" class="w3-bar-item w3-button w3-padding-large w3-hide-small">FAQ</a>
+				<a href="javascript:void(0)" class="w3-padding-large w3-hover-red w3-hide-small w3-right"><i class="fa fa-search"></i></a>
+			</div>
+		</div>
+		<div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
+			<a href="/HProducts" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Products</a>
+			<a href="/Contact" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">Contact Us</a>
+			<a href="/FAQ" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">FAQ</a>
+		</div>
 		
-			<!--Banner for website-->	
+</head>
 			
-		<header>
-		
-      <img class="feature" src = "https://i.imgur.com/uVymdir.png" height ="100" width ="100" title="Logo" alt="Logo">
-		<h1>Nuts &#38; Bolts</h1>
-		
-		  </div>
-		  </div>
-		  
-	</header>
-	
-	<!--Navigation Bar-->
-			<nav>
-	<ul>
-	<li><a href="HomePg.html">Home</a></li>
-	<li><a href="HProducts.html">HardCode Products</a></li>
-	<li><a href="ContUs.html">Contact Us</a></li>
-	<li><a href="FAQ.html">FAQ</a></li>
-	</ul>
-			</nav>
-			
-			<div id="wrapper">
+			<div id="w3-wrapper">
 	<!--Main Body for Website-->
 
 	<body>
