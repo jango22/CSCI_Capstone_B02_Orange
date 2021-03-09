729ee218ef4e08d5d09599894a6ada87f816e472
MasDijkstra <dijkstramas@gmail.com>
2021-03-08 20:00:04 -0500

    Update Products Functionality

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -15,14 +15,19 @@
         if(exist){
             alert(msg);
         }
-        
+        /* Enable forms script */
+        $(document).ready(function() {
+        $(".remove-attr").click(function(){            
+            $("input").removeAttr("disabled");
+        });
+    });
     </script>
     <style>
         h2 {text-indent: 10px;}
         .btn.btn-primary:disabled{background-color: #858585;}
     </style>
+    <?php $status = "disabled"; ?>
 </head>
-
 <body>
 <h2>Update a Product</h2>
 
@@ -56,98 +61,74 @@
         $searchSKU = $_POST['searchSKU'];
         $sql = $conn->query("SELECT * FROM INVENTORY WHERE productSKU = $searchSKU;");
         $data = $sql->fetchAll(PDO::FETCH_ASSOC)[0];
+
+        //Enable forms on the rest of the page
+        $status = "enabled";
     }
 ?>
-<script>
-/* Remove readonly element script */
-$(document).on(function(){
-            $("#searchsubmit").click(function(event){
-                event.preventDefault();
-                $("input").prop("disabled",false);
-                $("textarea").prop("disabled",false);
-                $("button").prop("disabled",false);
-            });
-        });
-</script>
 
 <form method="POST" enctype="multipart/form-data">
 @csrf
+    <input type="hidden" name="productID" value="<?php echo $data['productID'] ?? ''; ?>"/>
+
     <div class="form-group row">
         <label for="nameid" class="col-sm-1 col-form-label">Name:</label>
         <div class="col-sm-3">
-            <input type="text" class="form-control" name="name" id="nameid" placeholder="Product Name" maxlength="255" value="<?php echo $data['name'] ?? ''; ?>" required disabled>
+            <input type="text" class="form-control" name="name" id="nameid" placeholder="Product Name" maxlength="255" value="<?php echo $data['name'] ?? ''; ?>" required <?php echo $status; ?>>
         </div>
     </div>
 
     <div class="form-group row">
         <label for="SKUid" class="col-sm-1 col-form-label">SKU:</label>
         <div class="col-sm-3">
-            <input type="number" class="form-control" name="SKU" id="SKUid" placeholder="Product SKU" min="0" max="9999999999" value="<?php echo $data['productSKU'] ?? ''; ?>" required disabled>
+            <input type="number" class="form-control" name="SKU" id="SKUid" placeholder="Product SKU" min="0" max="9999999999" value="<?php echo $data['productSKU'] ?? ''; ?>" required <?php echo $status; ?>>
         </div>
     </div>
 
     <div class="form-group row">
         <label for="priceid" class="col-sm-1 col-form-label">Price:</label>
         <div class="col-sm-3">
-            <input type="number" step="0.01" class="form-control" name="price" placeholder="Price" id="priceid" min="0" max="9999999999" value="<?php echo $data['price'] ?? ''; ?>" required disabled>
+            <input type="number" step="0.01" class="form-control" name="price" placeholder="Price" id="priceid" min="0" max="9999999999" value="<?php echo $data['price'] ?? ''; ?>" required <?php echo $status; ?>>
         </div>
     </div>
 
     <div class="form-group row">
         <label for="descid" class="col-sm-1 col-form-label">Desc:</label>
         <div class="col-sm-3">
-            <textarea name="desc" class="form-control" rows="6" id="descid" placeholder="Description" maxlength="255" required disabled><?php echo $data['itemdesc'] ?? ''; ?></textarea>
+            <textarea name="desc" class="form-control" rows="6" id="descid" placeholder="Description" maxlength="255" required <?php echo $status; ?>><?php echo $data['itemdesc'] ?? ''; ?></textarea>
         </div>
     </div>
     
     <div class="form-group row">
         <label for="descid" class="col-sm-1 col-form-label"></label>
         <div class="offset-sm-3 col-sm-9 indent">
-            <button type="button" name="submitupdate" class="btn btn-primary" disabled>Update Product</button>
+            <button type="submit" name="submit" class="btn btn-primary" <?php echo $status; ?>>Update Product</button>
         </div>
     </div>
 </form>
 <br>
 
-
-
 <?php
+<<<<<<< Updated upstream
+=======
+    //Connect to database
+    $conn = new PDO("sqlsrv:Server=$servername,$port;Database=$dbname;", $username, $password); 
+
+>>>>>>> Stashed changes
     //Get values from POST
     if(isset($_POST['submit'])){
         $name = $_POST['name'];
         $SKU = $_POST['SKU'];
         $price = $_POST['price'];
         $desc = $_POST['desc'];
+        $productID = $_POST['productID'];
 
-        //Check for duplicate name
-        $sql = $conn->query("SELECT Name FROM INVENTORY;");
-        $names = $sql->fetchAll(PDO::FETCH_COLUMN);
-        $sql = $conn->query("SELECT ProductSKU FROM INVENTORY;");
-        $SKUs = $sql->fetchAll(PDO::FETCH_COLUMN);
-        if (in_array($name, $names)) {
-            $msg = "The name '$name' is already taken. Please choose a different one.";
-            echo "<div class='alert alert-danger alert-dismissable fade in' role='alert'>
-                    <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                    $msg
-                </div>";
-        }
-        //Check for duplicate SKU
-        elseif (in_array($SKU, $SKUs)) {
-            $msg = "The product SKU '$SKU' is already taken. Please choose a different one.";
-            echo "<div class='alert alert-danger alert-dismissable fade in' role='alert'>
-                    <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                    $msg
-                </div>";
-        } 
-        //Insert values into INVENTORY table
-        else {
-            $conn->query("INSERT INTO INVENTORY (name, productSKU, price, itemdesc) VALUES ('$name', $SKU, $price, '$desc');");
-            $msg = "Added to inventory successfully.";
-            echo "<div class='alert alert-success alert-dismissable fade in' role='alert'>
-                    <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-                    $msg
-                </div>";
-        }
+        $conn->query("UPDATE INVENTORY SET name='$name',productSKU='$SKU',price='$price',itemdesc='$desc' WHERE productID='$productID';");
+        $msg = "Added to inventory successfully.";
+        echo "<div class='alert alert-success alert-dismissable fade in' role='alert'>
+                <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+                $msg
+            </div>";
     }
 ?>
 
