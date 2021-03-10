3c0f4335cff4a36d01350c6dc30993bcb9e081bd
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-09 19:07:11 -0500

    Update HProducts.blade.php

--- resources/views/HProducts.blade.php
+++ resources/views/HProducts.blade.php
@@ -39,27 +39,27 @@
 			</div>
 			<input type="text" id="myInput" onkeyup="tableSearch()" placeholder="Search for products .." title="Type in a name">
 			<ul id="myUL" class="w3-ul w3-card-4" style="background-color:lightgray">
-				<li class="thumb w3-bar w3-hover-blue">
+				<li class="w3-bar w3-hover-blue">
 						<span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-xlarge w3-right">×</span>
-						<img src="https://i.imgur.com/N6Vx3Or.jpg" class="w3-bar-item w3-hide-small" style="width: 125px; height: 125px; ">
+						<img src="https://i.imgur.com/N6Vx3Or.jpg" class="w3-bar-item w3-hide-small" class="thumb" style="width: 125px; height: 125px; ">
 						<div class="w3-bar-item w3-left">
 							<h3>Hammer</h3>
 							<span>This is a hammer. May cause spontaneous bursts of lightning. Trust us, its a feature.</span> <br>
 							<span>$999.50</span>
 						</div>
 				</li>
-				<li class="thumb w3-bar w3-hover-red">
+				<li class="w3-bar w3-hover-red">
 						<span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-xlarge w3-right">×</span>
-						<img src="https://i.imgur.com/mFOndyT.jpeg" class="w3-bar-item w3-hide-small" style="width: 125px; height: 125px; ">
+						<img src="https://i.imgur.com/mFOndyT.jpeg" class="w3-bar-item w3-hide-small" class="thumb" style="width: 125px; height: 125px; ">
 						<div class="w3-bar-item" style="float:left;">
 							<h3>Glue</h3>
 							<span>If you want to bind two things together, use this. Seriously, they wont come apart. DISCLAIMER: Sniffing this stuff will drop you quicker than a Tyson right hook.</span><br>
 							<span>$40.00</span>
 						</div>
 				</li>
-				<li class="thumb w3-bar w3-hover-green">
+				<li class="w3-bar w3-hover-green">
 					<span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-xlarge w3-right">×</span>
-					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQexuifPTPeYz2hKSAbtGBhgU6NSGrQrtbh7w&usqp=CAU" class="w3-bar-item w3-hide-small" style="width:125px; height:125px;">
+					<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQexuifPTPeYz2hKSAbtGBhgU6NSGrQrtbh7w&usqp=CAU" class="thumb w3-bar-item w3-hide-small" style="width:125px; height:125px;">
 					<div class="w3-bar-item">
 						<h3>Light Bulb</h3>
 						<span>Okay, okay. It's not a light bulb, its techincally a lantern thats a ring. BUT! With this lantern/ring thing, all things are possible.</span>
@@ -67,10 +67,10 @@
 						<span>$00.25</span>
 					</div>
 				</li>
-				<li class="thumb w3-bar w3-hover-yellow">
+				<li class="w3-bar w3-hover-yellow">
 					
 					<span onclick="this.parentElement.style.display='none'" class="w3-bar-item w3-button w3-xlarge w3-right">×</span>
-					<img src="https://www.mccoys.com/images/8ecf916f-26e1-4a1a-9579-535c2d4c5943/400" class="w3-bar-item w3-hide-small" style="width:125px; height:125px;">
+					<img src="https://www.mccoys.com/images/8ecf916f-26e1-4a1a-9579-535c2d4c5943/400" class="thumb w3-bar-item w3-hide-small" style="width:125px; height:125px;">
 					<div class="w3-bar-item">
 						<h3>Just a 2x4</h3>
 						<span>Nothing to see here. It's a 2x4...</span><span class="w3-small">or is it?....</span>
