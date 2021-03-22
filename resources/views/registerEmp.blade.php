1bae0bb2f62f7999ba42d39f464033510b359c41
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-21 20:31:36 -0400

    Update registerEmp.blade.php

--- resources/views/registerEmp.blade.php
+++ resources/views/registerEmp.blade.php
@@ -25,11 +25,12 @@
 
     //Ensure password has one Capital, number, and special character
     if(isset($_POST['submit'])){
+        $username = $_POST['uname'];
+        $emp = "yes";
+        $pass = $_POST['pwd'];
+        $confirm = $_POST['cpwd'];
         if(isset($_POST['uname']) and !empty($_POST['uname']) and isset($_POST['pwd']) and !empty($_POST['pwd']) and isset($_POST['cpwd']) and !empty($_POST['cpwd'])) {
-            $username = $_POST['uname'];
-            $emp = "yes";
-            $pass = $_POST['pwd'];
-            $confirm = $_POST['cpwd'];
+            
             
             //regex patterns for password
             $numPat = "[0-9]";
