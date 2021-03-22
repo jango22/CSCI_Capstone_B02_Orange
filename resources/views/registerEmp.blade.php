344e76f7899e1cb14ca8751bf6c75661ddf1f094
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-21 20:04:32 -0400

    Update registerEmp.blade.php

--- resources/views/registerEmp.blade.php
+++ resources/views/registerEmp.blade.php
@@ -1,34 +1,4 @@
-﻿<!DOCTYPE html>
-<html>
-<style>
-    input[type=text], select {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        box-sizing: border-box;
-    }
-
-    input[type=submit] {
-        width: 100%;
-        background-color: slategrey;
-        color: white;
-        padding: 14px 20px;
-        margin: 8px 0;
-        border: none;
-        border-radius: 4px;
-        cursor: pointer;
-    }
-
-        input[type=submit]:hover {
-            background-color: lightslategray;
-        }
-</style>
-<body>
-
-    <h3>Using CSS to style an HTML Form</h3>
+  <h3>Using CSS to style an HTML Form</h3>
 
     <div>
         <form method="post" id="loginform">
@@ -92,8 +62,5 @@
             echo "you must set a username or password";
         }
     } 
-
-</body>
-</html>
 
 
