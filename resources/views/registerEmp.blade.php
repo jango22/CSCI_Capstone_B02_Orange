181747df7182ec28bf273b3359b9854f09107cf2
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-22 21:29:36 -0400

    Update registerEmp.blade.php

--- resources/views/registerEmp.blade.php
+++ resources/views/registerEmp.blade.php
@@ -30,7 +30,7 @@
             $upperLower = "[A-Za-z]";
             $special = "/[`'\"~!@#$*()<>\|]/";
             //checks username for lowercase
-          if (preg_match($upper, $uname) = 0) {
+          if (preg_match($upper, $uname)) {
 
             //checks if password has appropriate values
             if (preg_match($numPat, $pass) > 0 and preg_match($upperLower, $pass) > 0 and preg_match($special, $pass) > 0) {
