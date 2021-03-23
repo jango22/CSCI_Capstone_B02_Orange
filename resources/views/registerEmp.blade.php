0224cb320c8b408e3ec64ed828dc4c183aae0b6f
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-23 04:43:17 -0400

    Update registerEmp.blade.php

--- resources/views/registerEmp.blade.php
+++ resources/views/registerEmp.blade.php
@@ -23,40 +23,56 @@
     $port = "1433";
     $conn = new PDO("sqlsrv:Server=$servername,$port;Database=$dbname;", $user, $password);
 
- if(isset($_POST['submit'])){
-    if(isset($_POST['uname']) and !empty($_POST['uname']) and isset($_POST['pwd']) and !empty($_POST['pwd']) and isset($_POST['cpwd']) and !empty($_POST['cpwd'])) {
+if (isset($_POST['submit'])){
+    if (isset($_POST['uname']) and !empty($_POST['uname']) and isset($_POST['pwd']) and !empty($_POST['pwd']) and isset($_POST['cpwd']) and !empty($_POST['cpwd'])) {
             $username = $_POST['uname'];
             $emp = "yes";
             $pass = $_POST['pwd'];
             $confirm = $_POST['cpwd'];
-            
+       //checks username format
+       if (preg_match('~[A-Z]+~', $username)) {
+            echo "Your username cannot have uppercase!";
+       }
+       else if (preg_match('~[^a-zA-Z\d]+~', $username)) {
+            echo "Your username cannot have a special character";
+       }
+       else if (preg_match('~[\s]+~', $username)) {
+            echo "Your username cannot have whitespace!"
+       }
+       else {
 
             //checks if password has appropriate values
-            if (!preg_match('~[0-9]+~', $pass)) {
-
-                echo "Your password must contain a number. You must also have a lower case, upper case, and special character.";
-               
+            if (preg_match('~[0-9]+~', $pass)) {
+               echo "Your password must contain a number. You must also have a lower case, upper case, and special character.";               
             }
+             
             else if (!preg_match('~[A-Z]+~',$pass)) {
                 echo "Your password doesnt have a capital letter. You must also have a number, lower case letter and special character.";
             }
+            
             else if (!preg_match('~[a-z]+~',$pass)) {
                 echo "Your password doesnt have a lower case letter. You must also have a number, upper case letter and special character.";
             }
+            
             else if (!preg_match('~[^a-zA-Z\d]+~',$pass)) {
                 echo "Your password doesnt have a special character. You must also have a number, lower case, and upper case letter.";
             }
+            
             else {
                 
-                 if($pass == $confirm) {
+                if($pass == $confirm) {
                       $conn->query("INSERT INTO Users (username, password, is_Employee) VALUES ('$username', '$pass', '$emp')");
                 }
                 else {
                     echo "Passwords do not match!";
                 }
             }
-        }
-        else {
-            echo "you must set a username and password";
-        }
+            
+         }   
     }
+    
+    else {
+        echo "you must set a username and password";
+    }
+     
+}
