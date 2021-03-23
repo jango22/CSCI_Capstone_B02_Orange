95cce774a1b0f7be1b2aa85f6531f2150f2911f1
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-03-23 03:02:11 -0400

    Update registerEmp.blade.php

--- resources/views/registerEmp.blade.php
+++ resources/views/registerEmp.blade.php
@@ -43,7 +43,7 @@
             else if (!preg_match('~[a-z]+~',$pass)) {
                 echo "Your password doesnt have a lower case letter. You must also have a number, upper case letter and special character.";
             }
-            else if (!preg_match('~[/W]+~',$pass)) {
+            else if (!preg_match('~[^a-zA-Z\d]+~',$pass)) {
                 echo "Your password doesnt have a special character. You must also have a number, lower case, and upper case letter.";
             }
             else {
