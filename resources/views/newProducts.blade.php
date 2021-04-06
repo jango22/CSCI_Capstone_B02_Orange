cdd5c4b5610727fe0f525c96a93398928e8c5425
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-04-05 23:53:09 -0400

    Update newProducts.blade.php

--- resources/views/newProducts.blade.php
+++ resources/views/newProducts.blade.php
@@ -45,7 +45,7 @@ Add a Product
 	//if user isnt logged in, the session variable username gets set to guest
 	if(isset($_POST['addCart']) && isset($_SESSION['username'])) {
 		//check if the item being added is in stock
-		$conn->query('SELECT quantity FROM Inventory WHERE );
+		$conn->query('SELECT quantity FROM Inventory WHERE');
 		//check if a $_SESSION[cart] exists
 		//create one if not
 		//add the item or quantity to the cart, quantity increases if item exists
