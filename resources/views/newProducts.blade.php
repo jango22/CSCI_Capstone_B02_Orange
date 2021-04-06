d947bf549316080f49bb147e2aaca11ab49981d4
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-04-05 23:55:19 -0400

    Update newProducts.blade.php

--- resources/views/newProducts.blade.php
+++ resources/views/newProducts.blade.php
@@ -28,7 +28,7 @@ Add a Product
    
         <div class="w3-card"" style="background:lightgray">
         
-            <img src="https://i.imgur.com/uVymdir.png" alt="Product Image" style="width:100%">
+            
             <h1>{{ $product['name'] }}</h1><span>({{ $product['productSKU'] }})</span></br>
             <p class="price">${{number_format($product['price'], 2) }}</p>
             <p>{{ $product['itemdesc'] }}</p>
