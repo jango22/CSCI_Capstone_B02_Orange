7b97367c86afd47774dd1b4e6afb22eb95df14c0
MasDijkstra <dijkstramas@gmail.com>
2021-04-06 21:45:18 -0400

    Added navbar dropdown (needs work from someone who knows css lol)

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,12 +4,14 @@
 	<title>@yield('title')</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- Third-party scripts and stylesheets -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+	<!-- Third-party stylesheets -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" integrity="sha384-wGcDZTQxqZGwbGs+rK7nXEh5IxNHbeZ7ow3Mwv0wMkz0ZClwnb+OMG6qF+faMPPU" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+	<!-- Third-party scripts -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 
 	<!-- Our stylesheets -->
 	<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
@@ -25,19 +27,32 @@
 		<a href="/products" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-shopping-basket"></i>Products</a>
 		<a href="/contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-fw fa-envelope"></i>Contact Us</a>
 		<a href="/faq" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-question-circle"></i>FAQ</a>
-		<a href="/cart" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Cart</a>
+		<a href="/cart" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-shopping-cart"></i>Cart</a>
 
 		<?php 
 			if (isset($_SESSION['username'])) { 
 				if ($_SESSION['usertype'] == 'admin') {
 					/* Only show up for employee users */
-					echo '<a href="/registeremployee" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Register Employee</a>'; 
+
+					echo
+					'<div class="dropdown show w3-bar-item w3-button w3-padding-large w3-hide-small">
+						<a class="btn btn-secondary dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Employee Only<i class="fa fa-caret-down"></i>
+						</a>
+
+						<div class="dropdown-content" aria-labelledby="dropdownMenuLink">
+							<a href="/registeremployee" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Register Employee</a>
+							<a href="/add" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Add Product</a>
+							<a href="/update" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Update Product</a>
+						</div>
+					</div>';
+
+					/* echo '<a href="/registeremployee" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Register Employee</a>'; 
 					echo '<a href="/add" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Add Product</a>';
-					echo '<a href="/update" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Update Product</a>';
+					echo '<a href="/update" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Update Product</a>'; */
 
 				}
 				/* Only show up for users who are logged in */
-				echo '<a href="/cart" class="w3-bar-item w3-button w3-padding-large w3-hide-small">Cart</a>';
 				echo '<a href="/logout" class="w3-bar-item w3-button w3-padding-large w3-hide-small w3-right">Log Out</a>';
 				echo '<span href="" class="w3-bar-item w3-padding-large w3-hide-small w3-right">Welcome, ';
 				echo $_SESSION["fname"];
