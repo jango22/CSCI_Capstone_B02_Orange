18729eaeeb88b9e091133956d285377e3e42262e
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-04-19 23:02:47 -0400

    Update orderhistory.blade.php

--- resources/views/orderhistory.blade.php
+++ resources/views/orderhistory.blade.php
@@ -39,7 +39,7 @@ Products
             {
             echo "<li class="w3-bar">
                 <div id="product" class="w3-bar-item">
-                    <a href="/product?sku=<?php echo $transaction['orderID']; ?>"><span id="product-title">{{ $transaction['orderID'] }}</span></a><br> 
+                    <a href="/order?orderID=<?php echo $transaction['orderID']; ?>"><span id="product-title">{{ $transaction['orderID'] }}</span></a><br> 
                     Date: {{ $transaction['dateCreated'] }}<br>
                     Order Total: {{ $transaction['cartTotal']}}<br>
                 </div>
