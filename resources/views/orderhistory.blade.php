82115cf645294fd5ca8a789dd7b72684856050ab
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-04-20 11:16:25 -0400

    Update orderhistory.blade.php

--- resources/views/orderhistory.blade.php
+++ resources/views/orderhistory.blade.php
@@ -37,7 +37,7 @@ Products
         @foreach ($userHist as $transaction)
 		<li class='w3-bar'>
             <div id="product" class="w3-bar-item">
-                <a href="/ordert?orderID=<?php echo $transaction['orderID']; ?>"><span id="product-title">{{ $transaction['orderID'] }}</span></a></br>
+                <a href="/order?orderID=<?php echo $transaction['orderID']; ?>"><span id="product-title">{{ $transaction['orderID'] }}</span></a></br>
                 <?php
                     $orderID =  $transaction['orderID'];
                     $sql = $conn->query("SELECT DISTINCT dateCreated FROM Orders WHERE orderID =  '$orderID'");
