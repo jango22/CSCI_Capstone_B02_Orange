dc1b6307bffc48fd5254cfb8a5d74a0bd5bc56bd
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-04-27 00:04:26 -0400

    Update add.blade.php

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -105,7 +105,7 @@ else {
         } 
         //Insert values into INVENTORY table
         else {
-            $conn->query("INSERT INTO INVENTORY (name, productSKU, price, itemdesc, quantity, imageURL, category) VALUES ('$name', $SKU, $price, '$desc', $quant, '$image', $category);");
+            $conn->query("INSERT INTO INVENTORY (name, productSKU, price, itemdesc, quantity, imageURL, category) VALUES ('$name', $SKU, $price, '$desc', $quant, '$image', '$category');");
             $msg = "Added to inventory successfully.";
             echo "<div class='alert alert-success alert-dismissable fade in' role='alert'>
                     <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
