a9969bac5d7919495cbe3ec59bbff83ca59fad6a
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-04-26 23:49:04 -0400

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -32,12 +32,43 @@ Products
 	<div class="w3-card-4 w3-blue-gray">
 		<h2 class="w3-center">Products List</h2>
 	</div>
+    
+    //sorts the product list into categories
     @foreach ($inv as $cat)
+    $category = $cat['category'];
+    $sql = $conn->query("SELECT * FROM INVENTORY WHERE category = '$category'");
+	$search = $sql->fetchAll();
         <div class="w3-card-4 w3-blue-gray">
 		    <h2 class="w3-center">{{$cat['category']}}</h2>
 	    </div>
         <br>
+        @foreach ($search as $row)
+            <li class="w3-bar w3-hover-blue-gray">
+			<div id="product" class="w3-bar-item">
+				<!-- Product image -->
+				<div class="w3-bar-item w3-left">
+					@if ($product['imageURL'])
+						<img src="{{ $product['imageURL'] }}" alt="Product Image" style="width:80px;">
+					@else
+						<img src="https://i.imgur.com/h1VYuuO.png" alt="Product Image" style="width:80px;">
+					@endif
+				</div>
+				<!-- Product information -->
+				<div class="w3-bar-item w3-left">
+					@if ($product['quantity'] > 0)
+						<a href="/product?sku=<?php echo $product['productSKU']; ?>"><span id="product-title">{{ $product['name'] }}</span></a> <span>({{ $product['productSKU'] }})</span></br>
+					@else
+						<span id="product-title"><s>{{ $product['name'] }}</s></span>&nbsp;&nbsp;<span id="Out-of-stock">Out of Stock!</span></br>
+					@endif
+					<span>${{ number_format($product['price'], 2) }}</span></br>
+					<span>{{ $product['itemdesc'] }}</span><br>
+				</div>
+			</div>
+		    </li>
+        @endforeach
+		@endforeach
     @endforeach
+    
 	<br>
 
 	<ul id="products" class="w3-ul w3-card-4" style="background-color:lightgray">
