fd542bb1a4047ada7b346e233971182b2f73fcc8
Chitra Bhattarai <sabuschhetri@gmail.com>
2021-04-30 16:01:39 -0400

    Footer CSS

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -68,4 +68,10 @@ Contact Us
 
 	</div>
 </main>
+</div>
+<footer class="w3-blue-gray" style="padding:5px;text-align:center;">     
+  <p>Nuts and Bolts<br>
+  <a href="mailto:nutsandboltsb02@gmail.com">nutsandboltsb02@gmail.com</a></p>
+</footer>
+</div>
 @stop
--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -44,4 +44,10 @@ FAQ
     </div>
 </div>
 </main>
+</div>
+<footer class="w3-blue-gray" style="padding:5px;text-align:center;">     
+  <p>Nuts and Bolts<br>
+  <a href="mailto:nutsandboltsb02@gmail.com">nutsandboltsb02@gmail.com</a></p>
+</footer>
+</div>
 @stop
