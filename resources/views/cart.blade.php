fc7aeaca81fde2b102fedb3ebc0e4943c5260dba
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-05-06 01:47:06 -0400

    Update cart.blade.php

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -120,7 +120,7 @@ if (!empty($_POST)) {
                 
        try {
            $sql2 = $conn->query("SELECT code FROM Discount WHERE code = '$code'");
-           $check = $sql2->(PDO::FETCH_ASSOC)[0];
+           $check = $sql2->fetchAll(PDO::FETCH_ASSOC)[0];
            if ($code == $check['code']) {
                $sql3 = $conn->query("SELECT * FROM Discount WHERE code = '$code'");
                $off = $sql3->fetchAll();
