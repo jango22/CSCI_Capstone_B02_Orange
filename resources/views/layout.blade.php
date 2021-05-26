e30b93cee4e9a5782347a781bfa2e10ec25ef80b
Adaley2 <78830654+Adaley2@users.noreply.github.com>
2021-05-26 11:01:37 -0400

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,15 +32,6 @@
 		<a href="/faq" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-question-circle"></i>FAQ</a>
 		<a href="/cart" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-shopping-cart"></i>Cart</a>
         
-        <!-- Navbar items that are present when window size is too small -->
-        <div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
-		    <a href="/" class="w3-bar-item w3-button w3-padding-large"><i class="fa fa-fw fa-home"></i>Home</a>
-		    <a href="/products" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-shopping-basket"></i>Products</a>
-		    <a href="/contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-fw fa-envelope"></i>Contact Us</a>
-		    <a href="/faq" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-question-circle"></i>FAQ</a>
-		    <a href="/cart" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-shopping-cart"></i>Cart</a>
-        </div>
-        
 		<?php 
 			if (isset($_SESSION['username'])) { 
 				if ($_SESSION['usertype'] == 'admin') {
@@ -74,6 +65,14 @@
 		?>
 	</div>
 </div>
+<!-- Navbar items that are present when window size is too small -->
+        <div id="navDemo" class="w3-bar-block w3-black w3-hide w3-hide-large w3-hide-medium w3-top" style="margin-top:46px">
+		    <a href="/" class="w3-bar-item w3-button w3-padding-large"><i class="fa fa-fw fa-home"></i>Home</a>
+		    <a href="/products" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-shopping-basket"></i>Products</a>
+		    <a href="/contact" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-fw fa-envelope"></i>Contact Us</a>
+		    <a href="/faq" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-question-circle"></i>FAQ</a>
+		    <a href="/cart" class="w3-bar-item w3-button w3-padding-large w3-hide-small"><i class="fa fa-shopping-cart"></i>Cart</a>
+        </div>
 
 <body>
 @yield('content')
